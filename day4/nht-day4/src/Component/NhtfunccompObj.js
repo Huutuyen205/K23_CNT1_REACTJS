fd27b2b03efad82dff08e374babaa99919d310e8
Huutuyen205 <Huutuyen123tc@gmail.com>
2025-02-19 00:35:32 +0700

    day4

---
+++ day4/nht-day4/src/Component/NhtfunccompObj.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+export default function NhtfunccompObj(props) {
+  return (
+    <div>
+        <h2>props is object</h2>
+        <p><b>info:</b></p>
+        <p>Fullname: {props.renderInfo.fullname}</p>
+        <p>age: {props.renderInfo.age}</p>
+        <p>phone: {props.renderInfo.phone}</p>
+    </div>
+  )
+}
